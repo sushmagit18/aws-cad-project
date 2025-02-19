0385c07c8e4d57ff6ddfe902394042907ef3555a
sushmagit18 <gmsusshma2023@gmail.com>
2025-02-19 14:10:25 -0500

    Added

--- wordpress-app/bin/wordpress-app.ts
+++ wordpress-app/bin/wordpress-app.ts
@@ -8,7 +8,7 @@ const app = new cdk.App();
 
 const vpcDeployment = true;
 
-const deployment = { "stack" : "V" } 
+const deployment = { "stack" : "A" } 
 
 if (['V'].includes(deployment.stack)) {
     new WordpressvpcStack(app, 'WordpressvpcStack', {
